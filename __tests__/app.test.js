b5597ef1c7009a350c1ef9af8f867ba0c91037b3
ProsperieEli <ElijahProsperie@gmail.com>
2022-01-25 17:11:05 -0500

    finished app with passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Movie = require('../lib/models/Movie');
 
 describe('backend routes', () => {
   beforeEach(() => {
@@ -10,5 +11,73 @@ describe('backend routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should create a Movie', async () => {
+    const res = await request(app).post('/api/v1/movies').send({
+      title: 'Spider-Man',
+      release_year: 2020,
+      director: 'someone',
+    });
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title: 'Spider-Man',
+      releaseYear: 2020,
+      director: 'someone',
+    });
+  });
+
+  it('should get by id', async () => {
+    const movie = await Movie.insert({
+      title: 'Infinity War',
+      release_year: 2018,
+      director: 'someone',
+    });
+    const res = await request(app).get(`/api/v1/movies/${movie.id}`);
+
+    expect(res.body).toEqual(movie);
+  });
+
+  it('should get all movies', async () => {
+    const movie = await Movie.insert({
+      title: 'Infinity War',
+      release_year: 2018,
+      director: 'someone else',
+    });
+    const res = await request(app).get('/api/v1/movies');
+    expect(res.body).toEqual([movie]);
+  });
+
+  it('should update movie', async () => {
+    const movie = await Movie.insert({
+      title: 'Infinity War',
+      release_year: 2018,
+      director: 'someone else',
+    });
+    const res = await request(app).patch(`/api/v1/movies/${movie.id}`).send({
+      title: 'Infinity War',
+      release_year: 2018,
+      director: 'Anthony Russo',
+    });
+    const expectedMovie = {
+      id: expect.any(String),
+      title: 'Infinity War',
+      releaseYear: 2018,
+      director: 'Anthony Russo',
+    };
+    expect(res.body).toEqual(expectedMovie);
+    expect(await Movie.getById(movie.id)).toEqual(expectedMovie);
+  });
+
+  it('should delete', async () => {
+    const movie = await Movie.insert({
+      title: 'Infinity War',
+      release_year: 2018,
+      director: 'someone else',
+    });
+    const res = await request(app).delete(`/api/v1/movies/${movie.id}`);
+
+    expect(res.body).toEqual(movie);
+    expect(await Movie.getById(movie.id)).toBeNull();
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS movies
+DROP TABLE IF EXISTS movies;
 
 CREATE TABLE movies(
     id BIGINT GENERATED ALWAYS AS IDENTITY,
