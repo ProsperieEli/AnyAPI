1279d4ee2b70764ee64bfc29fe32b1c3bd8044b2
ProsperieEli <ElijahProsperie@gmail.com>
2022-01-24 20:21:49 -0500

    Movie Models done

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS movies
+
+CREATE TABLE movies(
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    title TEXT NOT NULL,
+    release_year INT NOT NULL,
+    director TEXT NOT NULL
+);
